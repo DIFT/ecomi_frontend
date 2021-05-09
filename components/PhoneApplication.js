93f62ad8ddc124fb75f490b75f043aa327622fcd
LK <lk@alienbuild.uk>
2021-05-09 22:54:03 +0100

    Truncate card

--- components/PhoneApplication.js
+++ components/PhoneApplication.js
@@ -277,7 +277,7 @@ const PhoneApplication = ({screen, setTab}) => {
                                 <div className={`text-center w-36 rounded-md overflow-hidden bg-gray-300  items-center justify-center flex`}>
                                     <div>
                                         <img src={`./assets/images/veve/tiles/featured/eduardo-risso_batman.jpg`} width={`100%`} className={`h-48 block`} />
-                                        <span className="block text-xs py-1 px-2 bg-gray-100 text-black text-left rounded-md">#86 Eduardo Risso Bat...</span>
+                                        <span className="block text-xs py-1 px-2 bg-gray-100 text-black text-left rounded-md">#86 Eduardo Risso...</span>
                                         <span className="inline-block text-xxs py-1 px-2 bg-gray-300 text-black text-left float-left">Ultra Rare</span>
                                         <span className="inline-block text-xxs py-1 px-2 bg-gray-300 text-black text-right float-right">
                                             <img src={`./assets/images/veve/icons/gem.png`} width={`13px`} className={`inline-block mr-1`} />
