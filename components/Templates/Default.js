a1a5efdc8b34ef9ecadebd2048d0951b68c6cacc
LK <lk@alienbuild.uk>
2021-09-10 23:53:22 +0100

    End of day commit

--- components/Templates/Default.js
+++ components/Templates/Default.js
@@ -15,7 +15,6 @@ const Default = ({ children }) => {
     return(
         <div className={controlOverflow ? 'overflow-hidden h-screen relative' : 'relative overflow-x-hidden'}>
             {/*TODO: Remove under construction when complete*/}
-            <span className={`uppercase absolute text-xs top-14 -right-12 text-black font-black bg-yellow-400 py-1 px-10 transform rotate-45 hidden md:block`}>Under construction</span>
             <Header setControlOverflow={setControlOverflow} />
             <main className={`pt-16`}>
                 <span className="radial-bg"></span>
